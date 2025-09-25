f2540ec838f9113bc93870033b8b5412f0a9ce62
nurkhairunisanisa355-cmd <nurkhairunisanisa355@gmail.com>
2025-09-25 11:28:18 +0700

    logika pembelian

--- penjualan.php
+++ penjualan.php
@@ -5,3 +5,23 @@ echo "Daftar Pembelian<br><br>";
 $nama_barang = ["teh pucuk", "alpokat", "roti", "jus jeruk", "citato"];
 $harga_barang = [6000, 2000, 3000, 7000, 4000];
 
+$beli = [];
+$jumlah = [];
+$total = [];
+$grandtotal = 0;
+
+// jumlah jenis barang yang dibeli random (2–5)
+$jenis_beli = rand(2, 5);
+
+// ambil index unik langsung dengan array_rand
+$barang_terpilih = array_rand($nama_barang, $jenis_beli);
+
+// loop untuk simpan hasil pembelian
+foreach($barang_terpilih as $index){  
+    $qty = rand(1,5); // jumlah beli random
+    
+    $beli[] = $index;
+    $jumlah[] = $qty;
+    $total[] = $harga_barang[$index] * $qty;
+}
+
