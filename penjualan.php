2078afb761f1fc56a663c049fc9c559af28a4cef
nurkhairunisanisa355-cmd <nurkhairunisanisa355@gmail.com>
2025-09-25 11:26:01 +0700

    setup awal

---
+++ penjualan.php
@@ -0,0 +1,7 @@
+<?php
+//Setup awal
+echo "--POLGAN MART--<br>";
+echo "Daftar Pembelian<br><br>";
+$nama_barang = ["teh pucuk", "alpokat", "roti", "jus jeruk", "citato"];
+$harga_barang = [6000, 2000, 3000, 7000, 4000];
+
